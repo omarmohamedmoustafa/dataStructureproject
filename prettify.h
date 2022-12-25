2f352fb9de9e10bdc159092a067113aecfcf3013
mohamdhassan0x7 <105478873+mohamdhassan0x7@users.noreply.github.com>
2022-12-25 18:13:04 +0200

    Create prettify.h

---
+++ prettify.h
@@ -0,0 +1,8 @@
+#pragma once
+#include<string>
+#include<vector>
+#include"structErrors.h"
+
+void singleTabCaseDefine(string line, int indexOpen);
+void tagDefine(string line);
+void prettify(string path, vector <string> &file);
