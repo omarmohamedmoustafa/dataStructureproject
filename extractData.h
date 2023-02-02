67c76c6a64d658e4ce857196bff108156b0d5ae0
Omar Mohamed <omar.studystuff@gmail.com>
2023-02-02 17:56:04 +0200

    Create extractData.h

---
+++ extractData.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+#include <queue>
+#include <fstream>
+#include<map>
+#include "userData.cpp"
+#include<algorithm>
+using namespace std;
+string minify(string path);
+string getTagData(int& i, string compData);
