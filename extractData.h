d69524b0a4c6e6116c3ae34e2dfe3e7750bcb525
ahmedessamrizk <105478629+ahmedessamrizk@users.noreply.github.com>
2023-02-02 17:58:51 +0200

    Update extractData.h
    Add function headers for analysis

--- extractData.h
+++ extractData.h
@@ -8,3 +8,15 @@
 using namespace std;
 string minify(string path);
 string getTagData(int& i, string compData);
+
+vector<_user> getData(string compData);
+
+
+_user m_Influencer(vector<_user> users);
+_user m_Active(vector<_user> users);
+
+
+vector<_user> mutualFollower(vector<_user> users, int id1, int id2);
+vector<_user> suggestions(vector<_user> users, int id);
+vector<pair<int, _post>> searchInPosts(string target);
+string JSON(vector<_user> users);
